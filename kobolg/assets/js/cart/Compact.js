b429b2de0d9ed4394c993e6ce943456afcbc1c81
ammarsaeed-beejo <80599563+ammarsaeed-beejo@users.noreply.github.com>
2021-11-12 19:07:10 +0200

    add firebase

--- kobolg/assets/js/cart/Compact.js
+++ kobolg/assets/js/cart/Compact.js
@@ -22,23 +22,8 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 getAnalytics(app);
 
-window.myFunction = myFunction;
-function myFunction() {
-    var input, filter, ul, li, a, i, txtValue;
-    input = document.getElementById("myInput");
-    filter = input.value.toUpperCase();
-    ul = document.getElementById("Compact");
-    li = ul.getElementsByTagName("li");
-    for (i = 0; i < li.length; i++) {
-        a = li[i].getElementsByTagName("a")[0];
-        txtValue = a.textContent || a.innerText;
-        if (txtValue.toUpperCase().indexOf(filter) > -1) {
-            li[i].style.display = "";
-        } else {
-            li[i].style.display = "none";
-        }
-    }
-}
+
+
 
 var products = [
     
